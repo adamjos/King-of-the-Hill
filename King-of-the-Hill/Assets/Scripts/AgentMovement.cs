3b216a877c06aff76301b87c7c2ef8a5bb91a0cb
adamjos <adam_josefsson@live.com>
2020-11-28 02:18:57 +0100

    Added initial environment and a simple controlable sphere

---
+++ King-of-the-Hill/Assets/Scripts/AgentMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    private float moveForce = 20f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float _horizontal = Input.GetAxis("Horizontal");
+        float _vertical = Input.GetAxis("Vertical");
+
+        Vector3 _moveDir = new Vector3(_horizontal, 0f, _vertical).normalized;
+
+        rb.AddForce(_moveDir * moveForce * Time.deltaTime, ForceMode.VelocityChange);
+    }
+}
